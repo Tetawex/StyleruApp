d4e3a3332617cb010ff38764fc486d2a3b0cc415
Dimagudilin <gudilin@styleru.org>
2017-03-03 14:10:54 +0300

    Первые идеи1

--- app/src/main/java/org/styleru/styleruapp/MainActivity.java
+++ app/src/main/java/org/styleru/styleruapp/MainActivity.java
@@ -1,5 +1,6 @@
 package org.styleru.styleruapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -49,27 +50,27 @@ public class MainActivity extends AppCompatActivity
 //        }
 //    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -80,7 +81,9 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_people) {
 
         } else if (id == R.id.nav_projects) {
-
+            Intent intent = new Intent(getApplication(),Projects.class);
+            startActivity(intent);
+            finish();
         } else if (id == R.id.nav_direct) {
 
         } else if (id == R.id.nav_events) {
