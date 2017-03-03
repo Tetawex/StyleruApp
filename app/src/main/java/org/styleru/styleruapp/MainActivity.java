2e0702d393349ea504c267c862aa7db04c7fa379
Dimagudilin <gudilin@styleru.org>
2017-03-03 10:27:14 +0300

    slidebar
    New slidebar

--- app/src/main/java/org/styleru/styleruapp/MainActivity.java
+++ app/src/main/java/org/styleru/styleruapp/MainActivity.java
@@ -77,13 +77,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_people) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_projects) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_direct) {
+
+        } else if (id == R.id.nav_events) {
 
 
         }
---
+++ app/src/main/java/org/styleru/styleruapp/Projects.java
@@ -0,0 +1,18 @@
+package org.styleru.styleruapp;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Пользователь on 03.03.2017.
+ */
+
+public class Projects extends AppCompatActivity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.projects);
+    }
+}
