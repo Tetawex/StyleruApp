153968963d51649c0f36a4a34afccb6e2c92d7fb
Tetawex <tetawex@gmail.com>
2017-03-07 21:54:54 +0300

    Revert "Revert "Реализован паттерн mvp для EventFeedFragment, есть бесконечная подгрузка новостей из тестовой модели.""
    This reverts commit a4946c599b36a423f3e76092e5a50c6e8cf18416.

--- app/src/main/java/org/styleru/styleruapp/view/EventsView.java
+++ app/src/main/java/org/styleru/styleruapp/view/EventsView.java
@@ -1,10 +1,15 @@
 package org.styleru.styleruapp.view;
 
+import org.styleru.styleruapp.model.dto.EventsItem;
+
+import java.util.List;
+
 /**
  * Created by tetawex on 06.03.17.
  */
 
 public interface EventsView extends View{
-    void appendData(int offset,int batchSize);
-    void updateData(int batchSize);
+    void appendData(List<EventsItem> data);
+    void setData(List<EventsItem> data);
+    void onDataUpdated();
 }
---
+++ app/src/main/java/org/styleru/styleruapp/view/adapter/recycler/BaseRecyclerAdapter.java
@@ -0,0 +1,63 @@
+package org.styleru.styleruapp.view.adapter.recycler;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Tetawex on 02.03.2017.
+ */
+
+public abstract class BaseRecyclerAdapter<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+{
+
+    protected Context context;
+    protected LayoutInflater inflater;
+    private List<T> data= Collections.emptyList();
+
+    public BaseRecyclerAdapter(Context context, List<T> data)
+    {
+        this.context=context;
+        inflater= LayoutInflater.from(context);
+        this.data=data;
+    }
+
+    public List<T> getData()
+    {
+        return data;
+    }
+
+    public void setData(List<T> data)
+    {
+        this.data = data;
+    }
+
+    public void setDataWithNotify(List<T> data)
+    {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    public void appendDataWithNotify(List<T> data)
+    {
+        if(this.data.equals(Collections.emptyList()))
+        {
+            setDataWithNotify(data);
+            return;
+        }
+        this.data.addAll(data);
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public int getItemCount()
+    {
+        //TODO: пофиксить костыль
+        return data == null ? 0 : data.size();
+    }
+}
