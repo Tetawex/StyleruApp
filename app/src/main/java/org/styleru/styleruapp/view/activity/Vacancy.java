9b301e1d1f8842948c4b4c091ae07c0b14b2c721
Dimagudilin <dima.gudilin@mail.ru>
2017-03-31 20:56:40 +0300

    Не пушить, бэкап

--- app/src/main/java/org/styleru/styleruapp/view/activity/Vacancy.java
+++ app/src/main/java/org/styleru/styleruapp/view/activity/Vacancy.java
@@ -2,7 +2,6 @@ package org.styleru.styleruapp.view.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
 import org.styleru.styleruapp.R;
@@ -21,13 +20,13 @@ public class Vacancy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vacancy);
 
-        back = (Button) findViewById(R.id.back);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+//        back = (Button) findViewById(R.id.back);
+//        back.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
 
 
     }
