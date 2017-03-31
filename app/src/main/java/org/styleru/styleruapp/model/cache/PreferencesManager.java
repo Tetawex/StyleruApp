1892bac9e83712000cf7666a7dc885650a6d7159
Tetawex <tetawex@gmail.com>
2017-03-31 18:05:38 +0300

    some dtos

---
+++ app/src/main/java/org/styleru/styleruapp/model/cache/PreferencesManager.java
@@ -0,0 +1,10 @@
+package org.styleru.styleruapp.model.cache;
+
+/**
+ * Created by Tetawex on 31.03.2017.
+ */
+
+public interface PreferencesManager {
+    String getAuthToken();
+    void setAuthToken(String token);
+}
---
+++ app/src/main/java/org/styleru/styleruapp/model/cache/PreferencesManagerAndroidImpl.java
@@ -0,0 +1,17 @@
+package org.styleru.styleruapp.model.cache;
+
+/**
+ * Created by Tetawex on 31.03.2017.
+ */
+
+public class PreferencesManagerAndroidImpl implements PreferencesManager {
+    @Override
+    public String getAuthToken() {
+        return null;
+    }
+
+    @Override
+    public void setAuthToken(String token) {
+
+    }
+}
